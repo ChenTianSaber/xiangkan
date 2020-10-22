9c6d55d36e75edc1783fd7c202fc2ac01e785f8e
陈添 <492934747@qq.com>
2020-10-23 00:04:05 +0800

    接入了友盟

--- app/src/main/java/com/chentian/xiangkan/XiangKanApplication.kt
+++ app/src/main/java/com/chentian/xiangkan/XiangKanApplication.kt
@@ -4,10 +4,20 @@ import android.app.Application
 import androidx.core.util.DebugUtils
 import com.blankj.utilcode.util.AppUtils
 import com.tencent.bugly.crashreport.CrashReport
+import com.umeng.analytics.MobclickAgent
+import com.umeng.commonsdk.UMConfigure
 
-class XiangKanApplication:Application() {
+class XiangKanApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         CrashReport.initCrashReport(this, "f1eb49d0fb", AppUtils.isAppDebug())
+        UMConfigure.init(
+            this,
+            "5f91a9f38a5de91db33dee20",
+            "CHEN_TIAN",
+            UMConfigure.DEVICE_TYPE_PHONE,
+            null
+        )
+        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
     }
 }
