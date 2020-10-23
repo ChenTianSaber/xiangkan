6f794723d9365d0cd938e854fb291aa7bab9e0bc
jerrychen <jerrychen@futunn.com>
2020-10-23 19:09:25 +0800

    改善了下管理页的样式，接下来优化一下样式，然后增加订阅up主的功能

---
+++ app/src/main/java/com/chentian/xiangkan/page/AddActivity.kt
@@ -0,0 +1,10 @@
+package com.chentian.xiangkan.page
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/chentian/xiangkan/page/manager/ManagerActivity.kt
+++ app/src/main/java/com/chentian/xiangkan/page/manager/ManagerActivity.kt
@@ -1,12 +1,15 @@
 package com.chentian.xiangkan.page.manager
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Window
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chentian.xiangkan.R
+import com.chentian.xiangkan.page.AddActivity
 import com.chentian.xiangkan.page.main.RSSListAdapter
 import com.chentian.xiangkan.utils.RSSInfoUtils
 import com.githang.statusbar.StatusBarCompat
@@ -16,17 +19,29 @@ class ManagerActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: ManagerListAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var back: ImageView
+    private lateinit var add: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_manager)
-        StatusBarCompat.setStatusBarColor(this, Color.WHITE,true)
+        StatusBarCompat.setStatusBarColor(this,  resources.getColor(R.color.white_3),true)
 
         initView()
     }
 
     private fun initView(){
+        back = findViewById(R.id.back)
+        back.setOnClickListener {
+            finish()
+        }
+
+        add = findViewById(R.id.add)
+        add.setOnClickListener {
+            startActivity(Intent(this,AddActivity::class.java))
+        }
+
         viewManager = LinearLayoutManager(this)
         viewAdapter = ManagerListAdapter()
         recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
